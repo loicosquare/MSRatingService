3d53dccc3ca9303c24f4c250396803297c9960d0
Loic Sanou <loicsan07@gmail.com>
2024-02-16 11:33:10 +0100

    Update Rating.java

--- src/main/java/com/tech/ratingService/entities/Rating.java
+++ src/main/java/com/tech/ratingService/entities/Rating.java
@@ -1,5 +1,6 @@
 package com.tech.ratingService.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document("user_ratings")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Rating {
     @Id
     private String ratingId;
